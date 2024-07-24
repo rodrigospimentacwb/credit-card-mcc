0dd6b1b8d6ea68ae23db97174844aafc711d351d
rodrigospimenta@gmail.com <rodrigospimenta@gmail.com>
2024-07-24 18:19:30 -0300

    Create from the initial project

---
+++ src/main/java/br/com/pepper/credit_card_mcc/CreditCardMccApplication.java
@@ -0,0 +1,13 @@
+package br.com.pepper.credit_card_mcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardMccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardMccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pepper/credit_card_mcc/CreditCardMccApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pepper.credit_card_mcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardMccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
