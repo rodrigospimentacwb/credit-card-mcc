243dbe9bf419d405e502d10cec4eec426ba36d53
rodrigospimenta@gmail.com <rodrigospimenta@gmail.com>
2024-07-24 21:57:04 -0300

    Add entity Account and AccountRepository
    Add migration and created one account with balance in database to use on L1 to L3 use cases.

---
+++ src/main/java/br/com/pepper/credit_card_mcc/repository/AccountRepository.java
@@ -0,0 +1,10 @@
+package br.com.pepper.credit_card_mcc.repository;
+
+import br.com.pepper.credit_card_mcc.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+}
